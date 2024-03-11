e2c9cf83e8efe113fb4b9a5eaf04723b1695034f
Javier <Javier@172.16.20.142>
2024-03-11 20:46:00 +0100

    Versión 1.1, cambio en el mensaje

--- ProyectoGit/src/paquete1/Principal.java
+++ ProyectoGit/src/paquete1/Principal.java
@@ -3,7 +3,7 @@ package paquete1;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Vean One Piece");
+		System.out.println("Vean One Piece, la mejor serie");
 	}
 
 }
