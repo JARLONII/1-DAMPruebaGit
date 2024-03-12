fb62a8daed862ac907e5b181a56ffa4a0bf3ee8b
Javier <Javier@172.16.20.142>
2024-03-12 16:04:55 +0100

    Añadida linea 7

--- ProyectoGit/src/paquete1/Principal.java
+++ ProyectoGit/src/paquete1/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Vean One Piece, la mejor serie");
+		System.out.println("Háganle caso, tiene razón");
 	}
 
 }
